a340ac80f7131fecd705906234be654b46c501db
Benjamin Tutu <tutu.benjamin65@gmail.com>
2026-04-14 21:02:01 +0000

    chaning admin creation logic to call funtion on start

--- app.py
+++ app.py
@@ -83,8 +83,11 @@ def inject_cart_count():
 
     return dict(cart_count=count)
 
+# create shop admin on start
 from command import create_admin
-app.cli.add_command(create_admin)
+with app.app_context():
+    create_admin()
+
 
 
 
